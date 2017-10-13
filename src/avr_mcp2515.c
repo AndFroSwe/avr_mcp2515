93d9e15be20d6e8e98b7645295dbab62ae2f0c64
andfroswe <gummianka88@gmail.com>
2017-10-13 22:16:22 +0200

    Have added a bunch of functions. SPI works. Workning on implementing functinos for MCP2515

--- src/avr_mcp2515.c
+++ src/avr_mcp2515.c
@@ -6,17 +6,25 @@
 
 #include "avr_mcp2515.h"
 
+
 int main(int argc, char *argv[])
 {
+    // Set pins
+    const io_pin spi_cs = {&PORTC, &DDRC, PC0};
+    pinmode(spi_cs, OUTPUT);
+
     // Start SPI
     spi_master_init();
+
+    // Initialize CAN
+    mcp2515_reset(spi_cs);
 
     // Send as test
     char c = 0;
     while (1)
     {
         _delay_ms(100);
-        spi_master_transmit(c++);
+        spi_master_readwrite(c++);
     }
 
 
@@ -24,6 +32,7 @@ int main(int argc, char *argv[])
     return 0;
 }
 
+/* SPI functions */
 void spi_master_init()
 {
     // Set MOSI and SCK output, all others input
@@ -34,10 +43,76 @@ void spi_master_init()
     SPCR |= _BV(SPE) | _BV(MSTR) | _BV(SPR0) |_BV(SPR1);
 }
 
-void spi_master_transmit(char c)
+uint8_t spi_master_readwrite(uint8_t cmd)
 {
     // Start transmission
-    SPDR = c;
+    SPDR = cmd;
     // Wait for transmission complete
     while(!(SPSR & _BV(SPIF)));
+
+    // Return read
+    return SPDR;
+}
+
+/* Pin manipulation functions */
+int pinmode(io_pin p, int s)
+{
+    if (s == INPUT)
+    {
+        *p.ddr &= ~_BV(p.pin);
+        return 0;
+    }
+    else if (s == OUTPUT)
+    {
+        *p.ddr |= _BV(p.pin);
+        return 0;
+    }
+    return -1; // Return fail if wrong input mode
+}
+int digital_write(io_pin p, int s)
+{
+    /* Set output pin high or low */
+    if (s == LOW)
+    {
+        *p.port &= ~_BV(p.pin);
+        return 0;
+    }
+    else if (s == HIGH)
+    {
+        *p.port |= _BV(p.pin);
+        return 0;
+    }
+    return -1; // Return fail if wrong input mode
+}
+
+/* MCP2515 functions */
+void mcp2515_reset(io_pin cs)
+{
+    // Reset mcp
+    digital_write(cs, LOW); // Start transaction
+    spi_master_readwrite(MCP2515_RESET);
+    digital_write(cs, HIGH);
+}
+void mcp2515_write_reg(uint8_t reg, uint8_t cmd, io_pin cs)
+{
+    // Write cmd to register reg
+    digital_write(cs, LOW);
+    _delay_us(CS_WAIT); // Wait for CS to settle
+    spi_master_readwrite(MCP2515_WRITE); // Initiate a write
+    spi_master_readwrite(reg); // Send register adress
+    spi_master_readwrite(cmd); // Send register value to write
+    digital_write(cs, HIGH); // Stop transaction
+}
+
+
+void mcp2515_bit_modify(uint8_t reg, uint8_t mask, uint8_t cmd, io_pin cs)
+{
+    // Modifies bit in register
+    digital_write(cs, LOW);
+    _delay_us(CS_WAIT); // Wait for CS to settle
+    spi_master_readwrite(MCP2515_BIT_MOD); // Initiate a write
+    spi_master_readwrite(reg); // Send register adress
+    spi_master_readwrite(mask); // Send mask
+    spi_master_readwrite(cmd); // Send register value to write
+    digital_write(cs, HIGH);
 }
--- src/avr_mcp2515.h
+++ src/avr_mcp2515.h
@@ -4,16 +4,50 @@
  * Description: Library for MCP2515 CAN network controller
  */
 
+/* Includes */
 #include <avr/io.h>
 #define F_CPU 8000000UL
 #include <util/delay.h>
 
-// Define SPI pins
+/* Defines */
+// SPI
 #define SCK PB5
 #define MISO PB4
 #define MOSI PB3
 #define SS PB2
 
-// Function declarations
+// Pins
+#define LOW 0
+#define HIGH 1
+#define INPUT 0
+#define OUTPUT 1
+
+// MCP2515
+#define CS_WAIT 10 // Delay time in us for CS to settle
+#define MCP2515_WRITE 0x02 // Write instruction command
+#define MCP2515_BIT_MOD 0x03 // Modify bit in register
+#define MCP2515_RESET 0x60 // Resets the state of mcp
+
+/* Typedefs */
+typedef struct Io_pin {
+    // Struct to make it easy to manipulate pins
+    volatile uint8_t *port; // Pin state port
+    volatile uint8_t *ddr;  // Pin direction register
+    int pin;                // Pin name
+} io_pin;
+
+
+/* Functions declarations */
+// SPI
 void spi_master_init();
-void spi_master_transmit(char c);
+uint8_t spi_master_readwrite(uint8_t msg); // Reads and write via SPI
+
+
+// Pins
+int pinmode(io_pin p, int s); // Set pin to be output of input
+int digital_write(io_pin p, int s); // Set digital pin. 0 for low, 1 for high
+
+// MCP2515
+void mcp2515_reset(io_pin cs);
+void mcp2515_write_reg(uint8_t reg, uint8_t cmd, io_pin cs);
+void mcp2515_bit_modify(uint8_t reg, uint8_t mask, uint8_t cmd, io_pin cs);
